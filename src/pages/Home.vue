f405460bb9454783aab370b99e533c0f8cda4546
ogeeDeveloper <omarogrant202@hotmail.com>
2023-08-28 19:18:44 -0500

    feat: allowed the routing to use the name routes for naviagating to the different pages

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -5,7 +5,7 @@
             <router-link 
                 v-for="destination in destinations"
                 :key="destination.id"
-                :to="destination.slug"
+                :to="{name: 'DestinationPage', params:{id: destination.id}}"
             >
                 <h2>{{ destination.name }}</h2>
                 <img :src="`/images/${destination.image}`" :alt="destination.name" >
